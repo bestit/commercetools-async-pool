dcbbf694abacae276e71eda56f8f88cc306f4ae2
Bjoern Lange <lange@bestit-online.de>
2017-03-22 22:34:48 +0100

    Added Unittesting for the clone method

--- tests/PoolTest.php
+++ tests/PoolTest.php
@@ -5,8 +5,13 @@ namespace BestIt\CTAsyncPool\Tests;
 use BestIt\CTAsyncPool\Pool;
 use BestIt\CTAsyncPool\PoolInterface;
 use Commercetools\Core\Client;
+use Commercetools\Core\Model\Customer\CustomerCollection;
+use Commercetools\Core\Request\Customers\CustomerQueryRequest;
+use Commercetools\Core\Response\ApiResponseInterface;
+use Commercetools\Core\Response\PagedQueryResponse;
 use PHPUnit\Framework\TestCase;
 use PHPUnit_Framework_MockObject_MockObject;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class PoolTest.
@@ -40,6 +45,21 @@ class PoolTest extends TestCase
     }
 
     /**
+     * Checks if the clone empties the promise queue.
+     * @return void
+     */
+    public function testCloneAndEmpty()
+    {
+        $this->fixture->addPromise($this->getMockedRequest());
+
+        static::assertCount(1, $this->fixture, 'There should be one promise.');
+
+        $clonedFixture = clone $this->fixture;
+
+        static::assertCount(0, $clonedFixture, 'After cloning there should be no promise.');
+    }
+
+    /**
      * Checks the default value for the count.
      * @covers Pool::count()
      * @return void
@@ -65,5 +85,33 @@ class PoolTest extends TestCase
     public function testInterface()
     {
         static::assertInstanceOf(PoolInterface::class, $this->fixture);
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockedRequest(): PHPUnit_Framework_MockObject_MockObject
+    {
+        $this->client
+            ->method('executeAsync')
+            ->with($request = static::createMock(CustomerQueryRequest::class))
+            ->will($this->returnValue($response1 = static::createMock(ApiResponseInterface::class)));
+
+        $response2 = static::createMock(ResponseInterface::class);
+
+        $request
+            ->method('buildResponse')
+            ->with($response2)
+            ->willReturn($response3 = static::createMock(PagedQueryResponse::class));
+
+        $request
+            ->method('mapResponse')
+            ->with($response3)
+            ->willReturn($response4 = static::createMock(CustomerCollection::class));
+
+        $response1
+            ->method('then')
+            ->willReturn($response3);
+        return $request;
     }
 }
